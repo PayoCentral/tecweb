c8fa82f3a632f2160ecabe999511ffc7935f5bf7
PayoCentral <ph836854@gmail.com>
2025-02-18 11:59:59 -0600

    XHTML: Definición y validación de documentos, practica 7

--- Practicas/p07/get_productos_xhtml.php
+++ Practicas/p07/get_productos_xhtml.php
@@ -2,79 +2,75 @@
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
     <title>Productos</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 </head>
 <body>
     <h3>PRODUCTOS</h3>
-    <br/>
+    <p></p>
 
     <?php
-    if(isset($_GET['tope']))
-    {
+    if (isset($_GET['tope'])) {
         $tope = $_GET['tope'];
-    }
-    else
-    {
+    } else {
         die('Parámetro "tope" no detectado...');
     }
 
-    if (!empty($tope))
-    {
-        /** SE CREA EL OBJETO DE CONEXION */
-        @$link = new mysqli('localhost', 'root', 'cinepolis', 'marketzone');    
+    if (!empty($tope)) {
+        /** SE CREA EL OBJETO DE CONEXIÓN */
+        @$link = new mysqli('localhost', 'root', 'cinepolis', 'marketzone');
 
-        /** comprobar la conexión */
-        if ($link->connect_errno) 
-        {
-            die('Falló la conexión: '.$link->connect_error.'<br/>');
+        /** Comprobar la conexión */
+        if ($link->connect_errno) {
+            die('Falló la conexión: ' . $link->connect_error . '<br />');
         }
 
         /** Crear una tabla que devuelve un conjunto de resultados */
-        if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") )  
-        {
-            echo "<table class='table'>
-                    <thead class='thead-dark'>
+        if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) {
+            echo '<table class="table">
+                    <thead class="thead-dark">
                         <tr>
-                        <th scope='col'>#</th>
-                        <th scope='col'>Nombre</th>
-                        <th scope='col'>Marca</th>
-                        <th scope='col'>Modelo</th>
-                        <th scope='col'>Precio</th>
-                        <th scope='col'>Unidades</th>
-                        <th scope='col'>Detalles</th>
-                        <th scope='col'>Imagen</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Unidades</th>
+                        <th scope="col">Detalles</th>
+                        <th scope="col">Imagen</th>
                         </tr>
                     </thead>
-                    <tbody>";
+                    <tbody>';
 
-            while ($row = $result->fetch_array(MYSQLI_ASSOC)) 
-            {
-                echo "<tr>
-                        <th scope='row'>".$row['id']."</th>
-                        <td>".$row['nombre']."</td>
-                        <td>".$row['marca']."</td>
-                        <td>".$row['modelo']."</td>
-                        <td>".$row['precio']."</td>
-                        <td>".$row['unidades']."</td>
-                        <td>".$row['detalles']."</td>
-                        <td><img src='".$row['imagen']."' alt='Imagen del producto'></td>
-                    </tr>";
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                echo '<tr>
+                        <th scope="row">' . $row['id'] . '</th>
+                        <td>' . $row['nombre'] . '</td>
+                        <td>' . $row['marca'] . '</td>
+                        <td>' . $row['modelo'] . '</td>
+                        <td>' . $row['precio'] . '</td>
+                        <td>' . $row['unidades'] . '</td>
+                        <td>' . $row['detalles'] . '</td>
+                        <td><img src="' . $row['imagen'] . '" alt="Imagen del producto" /></td>
+                    </tr>';
             }
 
-            echo "</tbody></table>";
+            echo '</tbody></table>';
 
             /** útil para liberar memoria asociada a un resultado con demasiada información */
             $result->free();
-        }
-        else
-        {
-            echo "No hay productos con esa cantidad de unidades.";
+        } else {
+            echo 'No hay productos con esa cantidad de unidades.';
         }
 
         $link->close();
     }
     ?>
+    
+    <p>
+    <a href="https://validator.w3.org/markup/check?uri=referer"><img
+      src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 </body>
 </html>
