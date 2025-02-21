a0542cd5deaa55ec11dc2f6aaf0915279321c95d
PayoCentral <ph836854@gmail.com>
2025-02-20 21:20:53 -0600

    P8: Hasta ejercicio 4/5

---
+++ Practicas/p08/set_productov2.php
@@ -0,0 +1,49 @@
+<?php
+
+$imagen   = 'img/imagen.png';
+
+/** SE CREA EL OBJETO DE CONEXION */
+@$link = new mysqli('localhost', 'root', 'cinepolis', 'marketzone');	
+
+/** comprobar la conexión */
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['name'];
+    $marca = $_POST['brand'];
+    $modelo = $_POST['model'];
+    $precio = $_POST['price'];
+    $detalles = $_POST['desc'];
+    $unidades = $_POST['unit'];
+
+    $sql = "SELECT * FROM productos WHERE nombre = '$nombre' AND marca = '$marca' AND modelo = '$modelo'";
+    $result = $link->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<p>Error: El producto ya existe en la base de datos.</p>";
+    } else {
+        $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
+        
+        if ($link->query($sql) === TRUE) {
+            echo 'Producto insertado con ID: '.$link->insert_id;
+            echo "<ul>
+                    <li>Nombre: $nombre</li>
+                    <li>Marca: $marca</li>
+                    <li>Modelo: $modelo</li>
+                    <li>Precio: $precio</li>
+                    <li>Descripción: $detalles</li>
+                    <li>Unidades: $unidades</li>
+                  </ul>";
+        } else {
+            echo "<p>Error al insertar el producto: " . $link->error . "</p>";
+        }
+    }
+}
+$link->close();
+?>
+
