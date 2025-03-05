df76faf775bf8d1989dea198823a215d4e80fa2f
PayoCentral <ph836854@gmail.com>
2025-03-04 18:48:35 -0600

    P10: Ejercicio 2

--- Practicas/p10/product_app/backend/read.php
+++ Practicas/p10/product_app/backend/read.php
@@ -1,29 +1,27 @@
 <?php
     include_once __DIR__.'/database.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    // Searchterm and likeTerm son para la búsqueda de productos, se usa LIKE para buscar coincidencias parciales
     $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
+    if( isset($_POST['searchTerm']) ) {
+        $searchTerm = $_POST['searchTerm'];
+        $query = "SELECT * FROM productos WHERE nombre LIKE ? OR marca LIKE ? OR detalles LIKE ?";
+        $stmt = $conexion->prepare($query);
+        $likeTerm = "%{$searchTerm}%";
+        $stmt->bind_param("sss", $likeTerm, $likeTerm, $likeTerm);
 
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = $value; // utf8_encode($value);
-                }
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $data[] = $row;
             }
-			$result->free();
-		} else {
+            $result->free();
+        } else {
             die('Query Error: '.mysqli_error($conexion));
         }
-		$conexion->close();
+        $stmt->close();
+        $conexion->close();
     } 
     
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
